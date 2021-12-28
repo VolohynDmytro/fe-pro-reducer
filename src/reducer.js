ff8c973f09958515615c4e70e5b48168b7b533b5
VolohynDmytro <voloshindima777@gmail.com>
2021-12-28 15:47:53 +0200

    CommRed

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,37 @@
+function upperCase(string) {
+  let words = string.split(' ');
+  let newArr = arr.map(function (word) {
+    return word.toUpperCase();
+  })
+  let result = newArr.join(' ');
+  return result;
+}
+
+function lowerCase(string) {
+  let words = string.split(' ');
+  let newArr = arr.map(function (word) {
+    return word.toLowerCase();
+  })
+  let result = newArr.join(' ');
+  return result;
+}
+const toCapitalize = string => {
+  const words = string.split(" ");
+  return words.map(word => word[0].toUpperCase() + word.slice(1)).join(" ")
+}
 function reducer(input, action) {
-  // Ваше решение тут
+  switch (action) {
+    case "upperCase":
+      return upperCase(input)
+      break
+    case "capitalize":
+      return toCapitalize(input)
+      break
+    case "lowerCase":
+      return lowerCase(input)
+      break
+  }
+  
 }
 
 module.exports = reducer;
